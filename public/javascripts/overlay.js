985ad0e9ed160c56d70323b8dfadc469bbce7a52
Aaron Santiago <aaronsantiago+github@gmail.com>
2021-05-05 21:12:36 -0400

    Make overlay toggle use a bool for cross-browser support

--- public/javascripts/overlay.js
+++ public/javascripts/overlay.js
@@ -4,9 +4,10 @@ $(document).ready(function () {
     new SimpleBar(document.getElementById('messagesContainer'));
 });
 
+let overlayOpen = false;
 
 function toggleOpen() {
-    if (document.getElementById('layover').style.transform === "translate(0%, 0%)") {
+    if (overlayOpen) {
         closeOverlay();
     } else {
         openOverlay();
@@ -14,11 +15,13 @@ function toggleOpen() {
 }
 
 function openOverlay() {
+    overlayOpen = true;
     document.getElementById('layover').style.transform = "translate(0%, 0%)";
     document.getElementById('tabArrow').style.transform = "rotate(180deg)";
 }
 
 function closeOverlay() {
+    overlayOpen = false;
     document.getElementById('layover').style.transform = "translate(-100%, 0%)";
     document.getElementById('tabArrow').style.transform = "rotate(0deg)";
 }
